3975b8211663754e1370d67fd7f29c0b98e57106
brooksquil <Apizza@203>
2024-05-23 16:26:37 -0500

    add jsx to tail wind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    './app/javascript/components/*.jsx'
   ]
+}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
